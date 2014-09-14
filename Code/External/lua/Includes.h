9cfe9f8459111d419ec0d4af306a5ebab61349fe
vinod <itzvnod.m@gmail.com>
2014-09-14 01:13:03 -0600

    Added LuaLib project and updated property macro files

---
+++ Code/External/lua/Includes.h
@@ -0,0 +1,14 @@
+/*
+	This file can be included by a project that wants to use Lua
+
+	Using this intermediate file to store the version number
+	means that no other files will have to change
+	if you ever change to a new version.
+*/
+
+#ifndef EAE6320_LUAINCLUDES_H
+#define EAE6320_LUAINCLUDES_H
+
+#include "5.2.3/src/lua.hpp"
+
+#endif	// EAE6320_LUAINCLUDES_H
